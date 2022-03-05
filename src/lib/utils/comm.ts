dcf4e6a19191d9b9b2d04270d7ed3c17723f44f2
Alexander <alexander@lindgren.xyz>
2022-03-05 11:55:32 +0100

    fix: move websocket else

--- src/lib/utils/comm.ts
+++ src/lib/utils/comm.ts
@@ -17,7 +17,11 @@ export default class Connection {
     constructor(channel: string, identifier: string) {
         this.identifier = identifier;
 
-        this.socket = new WebSocket("wss://demo.piesocket.com/v3/channel_1?api_key=oCdCMcMPQpbvNjUIzqtvF1d2X2okWpDQj4AwARJuAgtjhzKxVEjQU6IdCjwm&notify_self");
+        const url = new URL(window.location);
+        url.protocol = "wss"
+        url.pathname = "/ws"
+
+        this.socket = new WebSocket("wss://" + url +"test/websocket");
 
         this.socket.addEventListener('open', function (event) {
             console.log("Socket connection opened.");
